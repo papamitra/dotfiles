07622b373e8f1deb24e42a98f07703a9263138da
papamitra <papamitra@gmail.com>
2014-03-07 17:55:06 +0900

    Modify making symlink for dir

--- setup.sh
+++ setup.sh
@@ -6,7 +6,11 @@ DOT_FILES=( .zsh .zshrc .tmux.conf .emacs.d)
 
 for file in ${DOT_FILES[@]}
 do
-    ln -s $DOTFILES_DIR/$file $HOME/$file
+    if [ -d $DOTFILES_DIR/$file ]; then
+        ln -s $DOTFILES_DIR/$file $HOME
+    else
+        ln -s $DOTFILES_DIR/$file $HOME/$file
+    fi
 done
 
 git clone https://github.com/zsh-users/zaw.git $HOME/.zsh/zaw
