5be0258a7f8d5ac87af74ace9410f14e9e9a5632
papamitra <papamitra@gmail.com>
2014-03-07 16:08:28 +0900

    Modify yasnippet, init-loader

--- .emacs.d/inits/yasnippet.el
+++
@@ -1,7 +0,0 @@
-;; yasnippet
-(add-to-list 'load-path
-	     "~/.emacs.d/site-lisp/yasnippet")
-(require 'yasnippet) ;; not yasnippet-bundle
-(yas/initialize)
-(yas/load-directory "~/.emacs.d/site-lisp/yasnippet/snippets")
-(yas/load-directory "~/.emacs.d/snippets")
---
+++ .emacs.d/inits/yasnippet_init.el
@@ -0,0 +1,5 @@
+;; yasnippet
+(require 'yasnippet) ;; not yasnippet-bundle
+(yas-global-mode 1)
+;(yas/load-directory "~/.emacs.d/site-lisp/yasnippet/snippets")
+;(yas/load-directory "~/.emacs.d/snippets")
